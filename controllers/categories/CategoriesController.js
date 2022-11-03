2aa5ec50093a752842e2d37c31b74a979c1bc5e9
Breno <breno.am2004@gmail.com>
2022-11-03 15:20:57 -0300

    Rota de criação de categorias

--- controllers/categories/CategoriesController.js
+++ controllers/categories/CategoriesController.js
@@ -4,6 +4,9 @@ const express = require("express");
 
 // We'll use routes with the object "router" of the express
 const router = express.Router();
+const Category = require("./Category");
+// Using slugfy to transforme names/titles in slugs for the URLS (npm install --save slugify OR slugfy (I don't remember))
+const slugify = require("slugify");
 
 // router.[...] instead of app.[...]
 router.get("/categories", (req, res) => {
@@ -11,7 +14,32 @@ router.get("/categories", (req, res) => {
 });
 
 router.get("/admin/categories/new", (req, res) => {
-    res.send("Route to create new categories");
+    // res.send("Route to create new categories");
+
+    // Folder "views" is preselected by express
+    res.render("admin/categories/new");
+});
+
+// Method "post" to work with forms
+router.post("/admin/categories/save", (req, res) => {
+    // Route to save the new categories
+
+    var title = req.body.title;
+
+    if(title != undefined)
+    {
+        // Creating a new data with Category model (Category column)
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/");
+        })
+    }
+    else
+    {
+        res.redirect("/admin/categories/new");
+    }
 });
 
 module.exports = router;
