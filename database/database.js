082b9a776abef5e6224ee52c214eddd809aad317
Breno Moreira <breno.am2004@gmail.com>
2024-07-16 07:51:53 -0300

    att

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@
 const Sequelize = require("sequelize");
 
 // [...] = new Sequelize(database name, database user, database password, server [host: where's the server, dialect: what's the database])
-const connection = new Sequelize("guia_press", "root", "breno8anos", {host: 'localhost', dialect: 'mysql', timezone: '-03:00'});
+const connection = new Sequelize("guia_press", "root", "", {host: 'localhost', dialect: 'mysql', timezone: '-03:00'});
 
 // exporting the module 'connection' where the server is
 module.exports = connection;  
